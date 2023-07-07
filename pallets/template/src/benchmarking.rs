9437d91fc9ae6541a8cddc2e4e88cd9651392f13
cenwadike <chinedunwadike04@gmail.com>
2023-07-07 15:04:17 +0100

    added unit test and benchmarking test

--- pallets/template/src/benchmarking.rs
+++ pallets/template/src/benchmarking.rs
@@ -1,35 +1,31 @@
-// //! Benchmarking setup for pallet-template
-// #![cfg(feature = "runtime-benchmarks")]
-// use super::*;
+//! Benchmarking setup for pallet-template
+#![cfg(feature = "runtime-benchmarks")]
+use super::*;
 
-// #[allow(unused)]
-// use crate::Pallet as Template;
-// use frame_benchmarking::v2::*;
-// use frame_system::RawOrigin;
+#[allow(unused)]
+use crate::Pallet;
+use frame_benchmarking::v2::*;
+use frame_support::inherent::Vec;
+use frame_system::RawOrigin;
 
-// #[benchmarks]
-// mod benchmarks {
-// 	use super::*;
+#[benchmarks]
+mod benchmarks {
+	use super::*;
 
-// 	#[benchmark]
-// 	fn do_something() {
-// 		let value = 100u32.into();
-// 		let caller: T::AccountId = whitelisted_caller();
-// 		#[extrinsic_call]
-// 		do_something(RawOrigin::Signed(caller), value);
+	#[benchmark]
+	fn archive_book() {
+		// method params
+		let title: Vec<u8> = "title".into();
+		let author: Vec<u8> = "author".into();
+		let url: Vec<u8> = "url".into();
 
-// 		assert_eq!(Something::<T>::get(), Some(value));
-// 	}
+		// signed origin
+		let caller: T::AccountId = whitelisted_caller();
 
-// 	#[benchmark]
-// 	fn cause_error() {
-// 		Something::<T>::put(100u32);
-// 		let caller: T::AccountId = whitelisted_caller();
-// 		#[extrinsic_call]
-// 		cause_error(RawOrigin::Signed(caller));
+		// archive book entrisic call
+		#[extrinsic_call]
+		archive_book(RawOrigin::Signed(caller), title, author, url);
+	}
 
-// 		assert_eq!(Something::<T>::get(), Some(101u32));
-// 	}
-
-// 	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
-// }
+	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
+}
