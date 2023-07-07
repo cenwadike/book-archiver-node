4599b6d28c4871a8961cf35e36f700247f9e9877
cenwadike <chinedunwadike04@gmail.com>
2023-07-07 16:14:29 +0100

    added WeightInfo type in mock runtime

--- pallets/template/src/mock.rs
+++ pallets/template/src/mock.rs
@@ -51,6 +51,7 @@ impl system::Config for Test {
 
 impl pallet_template::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
 }
 
 // Build genesis storage according to the mock runtime.
